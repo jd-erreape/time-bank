8a419db4c67abe1f268a2d4e85e415f1a6cfcb83
Juan de Dios Herrero <juandediosherrero@gmail.com>
2013-06-10 23:43:59 +0200

    first-commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimeBank::Application.config.secret_key_base = '8b1db808082963b696a31924e5975ff0e66b96ece692040acfe624a185b6651b01d13c7f1102cffe37dba4ccc4bb08e362f4c8f28f61578d5f90c2a4c61127be'
