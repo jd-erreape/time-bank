a1801730fb776ed74d7c8f9e6573758399359a69
Francisco Lupión Villegas <fcolupionvillegas@gmail.com>
2013-06-18 20:42:03 +0200

    Searcher class with basic test not running

---
+++ test/models/searcher_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+require 'mocha/setup'
+
+class SearcherTest < ActiveSupport::TestCase
+  test 'search empty query' do
+    assert_equal Searcher.search(""), []
+  end
+
+  test 'search by title' do
+    favor_attributes = {:title => "Title"}
+    favor = Favor.new(favor_attributes)
+    assert_equal [favor], Searcher.search(favor_attributes[:title])
+  end
+
+end
+
+class Favor
+
+  attr_accessor :title
+
+  def self.initialize(attributes)
+    self.title=attributes[:title]
+  end
+
+end
+
+
+
