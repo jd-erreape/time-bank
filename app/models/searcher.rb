f6b2d043941954b0d16c03e1676d42c28c3df31a
Francisco Lupión Villegas <fcolupionvillegas@gmail.com>
2013-06-19 19:52:30 +0200

    Extract match method to an isolated class

--- app/models/searcher.rb
+++ app/models/searcher.rb
@@ -1,10 +1,15 @@
 class Searcher
 
-  def self.search(objects, query)
+  @@valid_searchable_attributes = [:title, :description]
 
+  def self.valid_searchable_attributes
+    @@valid_searchable_attributes
+  end
+
+  def self.search(objects, query)
     result = []
     objects.each do |object|
-      result << object if object.title.match(/.*#{query}.*/)
+      result << object if Matcher.match(object, valid_searchable_attributes, query)
     end
     result
   end
