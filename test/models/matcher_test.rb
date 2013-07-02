e957e89bfc2fe1cb13c6ed95754866edc37661d5
Francisco Lupión Villegas <fcolupionvillegas@gmail.com>
2013-07-02 20:33:48 +0200

    Extract match method to an isolated class

--- test/models/matcher_test.rb
+++ test/models/matcher_test.rb
@@ -1,30 +1,30 @@
 require 'test_helper'
 require 'mocha/setup'
 
-class MatcherTest < ActiveSupport::TestCase
+class MatcherSearcherTest < ActiveSupport::TestCase
   test 'match empty query' do
     object = stub(:title => "Title", :description => "Description")
-    assert_equal false, Matcher.match(object, Searcher.valid_searchable_attributes, "")
+    assert_equal false, MatcherSearcher.match(object, FavorSearcher.valid_searchable_attributes, "")
   end
 
   test 'match with title' do
     object = stub(:title => "Title", :description => "Description")
-    assert_equal true, Matcher.match(object, Searcher.valid_searchable_attributes, "Ti")
+    assert_equal true, MatcherSearcher.match(object, FavorSearcher.valid_searchable_attributes, "Ti")
   end
 
   test 'match with description' do
     object = stub(:title => "Title", :description => "Description")
-    assert_equal true, Matcher.match(object, Searcher.valid_searchable_attributes, "Desc")
+    assert_equal true, MatcherSearcher.match(object, FavorSearcher.valid_searchable_attributes, "Desc")
   end
 
   test 'doesnt match' do
     object = stub(:title => "Title", :description => "Description")
-    assert_equal false, Matcher.match(object, Searcher.valid_searchable_attributes, "Other")
+    assert_equal false, MatcherSearcher.match(object, FavorSearcher.valid_searchable_attributes, "Other")
   end
 
   test 'doesnt match without right fields' do
     object = stub(:title => "Title", :description => "Description")
-    assert_equal false, Matcher.match(object, Searcher.valid_searchable_attributes, "Other")
+    assert_equal false, MatcherSearcher.match(object, FavorSearcher.valid_searchable_attributes, "Other")
   end
 
 
